b1bb5c7410288f0cb263513024f729118edd2d14
Shoichi Sakane <github-shoichi@tanu.org>
2017-01-24 16:54:08 +0900

    queuing for workers is used.

--- pub_factory.c
+++ pub_factory.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <err.h>
 
+#include <sys/queue.h>
+
 #include <dslink/dslink.h>
 #define LOG_TAG "dsbench"	// dslink/log.h requires...
 #include <dslink/log.h>
@@ -24,27 +26,44 @@
 typedef struct {
 	DSLink *link;
 	DSNode *super_root;
-	/* loop = &link->loop; */
+       	struct work_queue_st *work_list;
 } base_loop_ctx_t;
 
-typedef struct {
+struct work_ctx_st {
+	TAILQ_ENTRY(work_ctx_st) link;
 	base_loop_ctx_t *base;
 	DSNode *node;
 	char *node_name;
+	uv_timer_t *timer;
 	void (*work_main)(uv_work_t *);
 	int working;
 	int finish;
 	char *string_value;
-} work_ctx_t;
+};
+typedef struct work_ctx_st work_ctx_t;
+
+TAILQ_HEAD(work_queue_st, work_ctx_st) work_queue;
 
 //=============
+char *
+work_util_setbuf_integer(int number)
+{
+	int buf_size = 5120;
+	char *buf = malloc(buf_size);
+	snprintf(buf, buf_size, "%d", number);
+	return buf;
+}
+
 void
 do_work_hard_each(uv_work_t *work)
 {
+	static int counter = 1;
 	work_ctx_t *wc = (work_ctx_t *)work->data;
 
+	wc->string_value = work_util_setbuf_integer(counter++);
+
 	if (f_debug) {
-		printf("%s", wc->node_name);
+		printf("%s%d", wc->node_name, counter);
 		fflush(stdout);
 	}
 }
@@ -57,10 +76,7 @@ do_heavy_work_for20s(uv_work_t *work)
 
 	usleep(2000000);
 
-	int buf_size = 10;
-	char *buf = malloc(buf_size);
-	snprintf(buf, buf_size, "%d", counter++);
-	wc->string_value = buf;
+	wc->string_value = work_util_setbuf_integer(counter++);
 
 	if (f_debug) {
 		printf("%s", wc->node_name);
@@ -80,10 +96,7 @@ do_heavy_work_for30s(uv_work_t *work)
 
 	usleep(3000000);
 
-	int buf_size = 10;
-	char *buf = malloc(buf_size);
-	snprintf(buf, buf_size, "%d", counter++);
-	wc->string_value = buf;
+	wc->string_value = work_util_setbuf_integer(counter++);
 
 	if (f_debug) {
 		printf("%s", wc->node_name);
@@ -99,6 +112,8 @@ do_heavy_work_for30s(uv_work_t *work)
 void
 work_ctx_destroy(work_ctx_t *wc)
 {
+	TAILQ_REMOVE(wc->base->work_list, wc, link);
+
 	if (wc->string_value != NULL)
 		free(wc->string_value);
 
@@ -108,7 +123,7 @@ work_ctx_destroy(work_ctx_t *wc)
 
 work_ctx_t *
 work_ctx_new(base_loop_ctx_t *base, DSNode *node, char *node_name,
-		void (*work_main)(uv_work_t *))
+		uv_timer_t *timer, void (*work_main)(uv_work_t *))
 {
 	work_ctx_t *wc = malloc(sizeof(*wc));
 	if (wc == NULL) {
@@ -118,10 +133,13 @@ work_ctx_new(base_loop_ctx_t *base, DSNode *node, char *node_name,
 	wc->base = base;
 	wc->node = node;
 	wc->node_name = strdup(node_name);
+	wc->timer = timer;
 	wc->work_main = work_main;
 	wc->working = 0;
 	wc->finish = 0;
 	wc->string_value = NULL;
+
+	TAILQ_INSERT_TAIL(wc->base->work_list, wc, link);
 
 	return wc;
 }
@@ -137,11 +155,12 @@ queue_work_finished(uv_work_t *work, int status)
 	if (wc->string_value) {
 		json_t *j_val = json_string(wc->string_value);
 
-		if (dslink_node_set_value(wc->base->link, wc->node, j_val) != 0) {
-			log_warn("failed to set the value of %s\n", wc->node_name);
+		if (dslink_node_set_value(wc->base->link,
+					wc->node, j_val) != 0) {
+
+			log_warn("failed to set the value of %s\n",
+					wc->node_name);
 			json_decref(j_val);
-			dslink_node_tree_free(wc->base->link, wc->node);
-			return;
 		}
 
 		free(wc->string_value);
@@ -158,10 +177,11 @@ queue_work_start(work_ctx_t *wc)
 {
 	uv_work_t *work = malloc(sizeof(*work));
 	work->data = wc;
+
 	uv_queue_work(&wc->base->link->loop, work, wc->work_main,
 			queue_work_finished);
-	wc->working++;
 
+	wc->working++;
 	if (f_debug) {
 		printf("+");
 		fflush(stdout);
@@ -172,7 +192,33 @@ void
 heavy_work_timer_close(uv_handle_t *handle)
 {
 	uv_timer_t *timer = (uv_timer_t *)handle;
+	work_ctx_t *wc = (work_ctx_t *)timer->data;
+
 	free(timer);
+	work_ctx_destroy(wc);
+}
+
+void
+heavy_work_stop(work_ctx_t *wc)
+{
+	if (f_debug) {
+		printf("%s has been stopped.", wc->node_name);
+		fflush(stdout);
+	}
+	uv_timer_stop(wc->timer);
+	uv_close((uv_handle_t *)wc->timer, heavy_work_timer_close);
+}
+
+void
+heavy_work_stop_all(base_loop_ctx_t *base)
+{
+	work_ctx_t *wc;
+
+	while (TAILQ_FIRST(base->work_list) != NULL) {
+		wc = TAILQ_FIRST(base->work_list);
+		TAILQ_REMOVE(base->work_list, wc, link);
+		heavy_work_stop(wc);
+	}
 }
 
 static void
@@ -185,20 +231,15 @@ on_heavy_work_timer(uv_timer_t *timer)
 	 */
 	if (wc->finish) {
 		/* shutdown */
-		log_info("%s done\n", wc->node_name);
-		uv_timer_stop(timer);
-		uv_close((uv_handle_t *)timer, heavy_work_timer_close);
-		work_ctx_destroy(wc);
+		heavy_work_stop(wc);
 		return;
 	}
 
 	/*
 	 * check whether this task has been working already.
 	 */
-#if 1
-	if (wc->working)
+	if (wc->working > 0)
 		return;
-#endif
 
 	queue_work_start(wc);
 }
@@ -236,7 +277,7 @@ heavy_work_start(base_loop_ctx_t *base, char *node_name, useconds_t interval,
 	 */
 	uv_timer_t *timer = malloc(sizeof(*timer));
 	uv_timer_init(&base->link->loop, timer);
-	timer->data = work_ctx_new(base, node, node_name, work_main);
+	timer->data = work_ctx_new(base, node, node_name, timer, work_main);
 	uv_timer_start(timer, on_heavy_work_timer, 0, interval);
 
 	return 0;
@@ -254,8 +295,20 @@ base_loop_ctx_new(DSLink *link, DSNode *super_root)
 	base_loop_ctx_t *base = malloc(sizeof(*base));
 	base->link = link;
 	base->super_root = super_root;
+	base->work_list = malloc(sizeof(*base->work_list));
 
 	return base;
+}
+
+static int
+factory_shutdown(DSLink *link)
+{
+	base_loop_ctx_t *base = (base_loop_ctx_t *)link->loop.data;
+
+	heavy_work_stop_all(base);
+	free(base);
+
+	return 0;
 }
 
 static int
@@ -263,12 +316,20 @@ factory_init(DSLink *link, DSNode *super_root)
 {
 	base_loop_ctx_t *base = base_loop_ctx_new(link, super_root);
 
+	link->loop.data = base;
+
+	TAILQ_INIT(base->work_list);
+
 	/*
 	 * here create timers for the threads working heavy.
 	 */
+#if 0
 	heavy_work_start(base, "A", 100, do_heavy_work_for20s);
 	heavy_work_start(base, "B", 200, do_heavy_work_for30s);
 	heavy_work_start(base, "C", 300, do_work_hard_each);
+#else
+	heavy_work_start(base, "C", 10, do_work_hard_each);
+#endif
 
 	return 0;
 }
@@ -276,9 +337,6 @@ factory_init(DSLink *link, DSNode *super_root)
 static void
 pub_on_init(DSLink *link)
 {
-	if (factory_init(link, link->responder->super_root) < 0)
-		errx(1, "ERROR: factory_init() failed.");
-
 	log_info("Initialized!\n");
 }
 
@@ -286,12 +344,17 @@ static void
 pub_on_connected(DSLink *link)
 {
 	log_info("Connected!\n");
+
+	if (factory_init(link, link->responder->super_root) < 0)
+		errx(1, "ERROR: factory_init() failed.");
 }
 
 static void
 pub_on_disconnected(DSLink *link)
 {
 	log_info("Disconnected!\n");
+
+	factory_shutdown(link);
 }
 
 int
